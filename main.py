1a452b99b06759cf6ede44384ab8f7cf9885ec53
Prateek Kumar <prateek21081@iiitd.ac.in>
2024-10-29 00:40:48 +0530

    add more user methods

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from sre_constants import MARK
 from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
+MAX_SKIP = 10
 DH_PARAMETERS = dh.generate_parameters(generator=2, key_size=2048)
 
 def GENERATE_DH() -> tuple[dh.DHPrivateKey, dh.DHPublicKey]:
@@ -21,7 +23,7 @@ def DH(dh_pair: tuple[dh.DHPrivateKey, dh.DHPublicKey], dh_pub: dh.DHPublicKey):
 
 def KDF_CK(CK):
     h_ck = hmac.HMAC(CK, hashes.SHA256())
-    h_mk = h_ck.copy()
+    h_mk = hmac.HMAC(CK, hashes.SHA256())
     h_ck.update(b'\x01')
     h_ck.update(b'\x02')
     chain_key = h_ck.finalize()
@@ -49,11 +51,54 @@ class User():
         self.Ns = 0
         self.Nr = 0
         self.PN = 0
-        self.MSKIPPED = dict()
+        self.MKSKIPPED = dict()
 
     def dh(self, peer_dh_public_key: dh.DHPublicKey):
         self.DHr = peer_dh_public_key
         self.SK = DH(self.DHs, self.DHr)
+
+    def ratchet_encrypt(self, plaintext, AD):
+        self.CKs, mk = KDF_CK(self.CKs)
+        header = HEADER(self.DHs, self.PN, self.Ns)
+        self.Ns += 1
+        return header, ENCRYPT(mk, plaintext, CONCAT(AD, header))
+    
+    def ratchet_decrypt(self, header, ciphertext, AD):
+        plaintext = self.try_skipped_message_keys(header, ciphertext, AD)
+        if plaintext != None:
+            return plaintext
+        if header.dh != self.DHr:
+            self.skip_message_keys(header.pn)
+            self.dh_ratchet(header)
+        self.skip_message_keys(header.n)
+        self.CKr, mk = KDF_CK(self.CKr)
+        self.Nr += 1
+        return DECRYPT(mk, ciphertext, CONCAT(AD, header))
+
+    def try_skipped_message_keys(self, header, ciphertext, AD):
+        if (header.dh, header.n) not in self.MKSKIPPED:
+            return None
+        mk = self.MKSKIPPED[header.dh, header.n]
+        del self.MKSKIPPED[header.dh, header.n]
+        return DECRYPT(mk, ciphertext, CONCAT(AD, header))
+
+    def skip_message_keys(self, until):
+        if self.Nr + MAX_SKIP < until:
+            raise RuntimeError()
+        if self.CKr != None:
+            while self.Nr < until:
+                self.CKr, mk = KDF_CK(self.CKr)
+                self.MKSKIPPED[self.DHr, self.Nr] = mk
+                self.Nr += 1
+
+    def dh_ratchet(self, header):
+        self.PN = self.Ns
+        self.Ns = 0
+        self.Nr = 0
+        self.DHr = header.dh
+        self.RK, self.CKr = KDF_RK(self.RK, DH(self.DHs, self.DHr))
+        self.DHs = GENERATE_DH()
+        self.RK, self.CKs = KDF_RK(self.RK, DH(self.DHs, self.DHr))
 
 if __name__ == "__main__":
     alice = User()
