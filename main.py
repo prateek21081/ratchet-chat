a819dad2c388e28f95e9362578f1858e278587cd
Prateek Kumar <prateek21081@iiitd.ac.in>
2024-11-06 19:38:06 +0530

    add docs for double ratchet external functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pickle
+import json
+from dataclasses import dataclass
 from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
@@ -8,15 +9,18 @@ from cryptography.hazmat.primitives.padding import PKCS7
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 from cryptography.hazmat.backends import default_backend
 
-MAX_SKIP = 10
+MAX_SKIP = 10 # Maximum number of message keys that can be skipped in a single chain.
 DH_PARAMETERS = dh.generate_parameters(generator=2, key_size=2048)
 
 def GENERATE_DH() -> tuple[dh.DHPrivateKey, dh.DHPublicKey]:
+    """Returns a new Diffie-Hellman key pair."""
     private_key = DH_PARAMETERS.generate_private_key()
     public_key = private_key.public_key()
     return (private_key, public_key)
 
-def DH(dh_pair: tuple[dh.DHPrivateKey, dh.DHPublicKey], dh_pub: dh.DHPublicKey):
+def DH(dh_pair: tuple[dh.DHPrivateKey, dh.DHPublicKey], dh_pub: dh.DHPublicKey) -> bytes:
+    """Returns the output from the Diffie-Hellman calculation between the
+    private key from the DH key pair dh_pair and the DH public key dh_pub."""
     shared_key = dh_pair[0].exchange(dh_pub)
     derived_key = HKDF(
         algorithm=hashes.SHA256(),
@@ -27,7 +31,9 @@ def DH(dh_pair: tuple[dh.DHPrivateKey, dh.DHPublicKey], dh_pub: dh.DHPublicKey):
     ).derive(shared_key)
     return derived_key
 
-def KDF_CK(CK):
+def KDF_CK(CK: bytes) -> tuple[bytes, bytes]:
+    """Returns a pair (32-byte chain key, 32-byte message key) as the output of
+    applying a KDF keyed by a 32-byte chain key ck to some constant."""
     h_ck = hmac.HMAC(CK, hashes.SHA256())
     h_mk = hmac.HMAC(CK, hashes.SHA256())
     h_ck.update(b'\x01')
@@ -36,7 +42,10 @@ def KDF_CK(CK):
     msg_key = h_mk.finalize()
     return chain_key, msg_key
 
-def KDF_RK(RK, dh_out):
+def KDF_RK(RK: bytes, dh_out: bytes) -> tuple[bytes, bytes]:
+    """Returns a pair (32-byte root key, 32-byte chain key) as the output of
+    applying a KDF keyed by a 32-byte root key rk to a Diffie-Hellman output
+    dh_out."""
     key = HKDF(
         algorithm=hashes.SHA256(),
         length=64,
@@ -48,10 +57,15 @@ def KDF_RK(RK, dh_out):
     msg_key = key[32:]
     return chain_key, msg_key
 
-def CONCAT(ad, header):
-    return ad + pickle.dumps(header)
+def CONCAT(ad: bytes, header: dict) -> bytes:
+    """Encodes a message header into a parseable byte sequence, prepends the ad
+    byte sequence, and returns the result."""
+    return ad + bytes(json.dumps(header))
 
-def HEADER(dh_pair: tuple[dh.DHPrivateKey, dh.DHPublicKey], pn, n):
+def HEADER(dh_pair: tuple[dh.DHPrivateKey, dh.DHPublicKey], pn: int, n: int) -> dict:
+    """Creates a new message header containing the DH ratchet public key from
+    the key pair in dh_pair, the previous chain length pn, and the message
+    number n."""
     header = {
         'dh': dh_pair[1].public_bytes(encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo),
         'pn': pn,
@@ -59,8 +73,8 @@ def HEADER(dh_pair: tuple[dh.DHPrivateKey, dh.DHPublicKey], pn, n):
     }
     return header
 
-
-def ENCRYPT(mk, plaintext, associated_data):
+def ENCRYPT(mk: bytes, plaintext: bytes, associated_data: bytes) -> bytes:
+    """Returns an AEAD encryption of plaintext with message key mk."""
     key = HKDF(
         algorithm=hashes.SHA256(),
         length=80,
@@ -77,7 +91,8 @@ def ENCRYPT(mk, plaintext, associated_data):
     # TODO: add HMAC operation.
     return ciphertext
 
-def DECRYPT(mk, ciphertext, associated_data):
+def DECRYPT(mk: bytes, ciphertext: bytes, associated_data: bytes):
+    """Returns the AEAD decryption of ciphertext with message key mk."""
     key = HKDF(
         algorithm=hashes.SHA256(),
         length=80,
@@ -94,22 +109,35 @@ def DECRYPT(mk, ciphertext, associated_data):
     # TODO: add HMAC operation.
     return plaintext
 
+@dataclass
+class Header():
+    dh: bytes
+    pn: int
+    n: int
+
 class User():
-    def __init__(self, username:str = ''):
-        self.username = username
+    def __init__(self):
+        # DH Ratchet key pair (the "sending" or "self" ratchet key)
+        self.DHs = GENERATE_DH()
+        # DH Ratchet public key (the "received" or "remote" key)
+        self.DHr = b'' 
+        # 32-byte Root Key
+        self.RK = b'' 
+        # 32-byte Chain Key for sending and receiving
+        self.CKs = b'' 
+        self.CKr = b''
+        # Message numbers for sending and receiving
+        self.Ns = 0
+        self.Nr = 0
+        # Number of messages in previous sending chain
+        self.PN = 0 
+        # Dictionary of skipped-over message keys, indexed by ratchet public
+        # key and message number.
+        self.MKSKIPPED = dict() 
+
         self.IK = X25519PrivateKey.generate()
         self.SPK = X25519PrivateKey.generate()
         self.SK = b''
-
-        self.DHs = GENERATE_DH()
-        self.DHr = None
-        self.RK = b''
-        self.CKs = b''
-        self.CKr = b'' 
-        self.Ns = 0
-        self.Nr = 0
-        self.PN = 0
-        self.MKSKIPPED = dict()
 
     def perform_x3dh_start(self, IKr: X25519PublicKey, SPKr: X25519PublicKey):
         EK = X25519PrivateKey.generate()
@@ -141,13 +169,13 @@ class User():
         self.DHr = peer_dh_public_key
         self.SK = DH(self.DHs, self.DHr)
 
-    def ratchet_encrypt(self, plaintext, AD):
+    def ratchet_encrypt(self, plaintext: bytes, AD: bytes):
         self.CKs, mk = KDF_CK(self.CKs)
         header = HEADER(self.DHs, self.PN, self.Ns)
         self.Ns += 1
         return header, ENCRYPT(mk, plaintext, CONCAT(AD, header))
     
-    def ratchet_decrypt(self, header, ciphertext, AD):
+    def ratchet_decrypt(self, header, ciphertext: bytes, AD: bytes):
         plaintext = self.try_skipped_message_keys(header, ciphertext, AD)
         if plaintext != None:
             return plaintext
@@ -159,14 +187,14 @@ class User():
         self.Nr += 1
         return DECRYPT(mk, ciphertext, CONCAT(AD, header))
 
-    def try_skipped_message_keys(self, header, ciphertext, AD):
+    def try_skipped_message_keys(self, header, ciphertext: bytes, AD: bytes):
         if (header['dh'], header['n']) not in self.MKSKIPPED:
             return None
         mk = self.MKSKIPPED[header['dh'], header['n']]
         del self.MKSKIPPED[header['dh'], header['n']]
         return DECRYPT(mk, ciphertext, CONCAT(AD, header))
 
-    def skip_message_keys(self, until):
+    def skip_message_keys(self, until: int):
         if self.Nr + MAX_SKIP < until:
             raise RuntimeError()
         if self.CKr != None:
@@ -185,8 +213,8 @@ class User():
         self.RK, self.CKs = KDF_RK(self.RK, DH(self.DHs, self.DHr))
 
 if __name__ == "__main__":
-    alice = User("Alice")
-    bob = User("Bob")
+    alice = User()
+    bob = User()
 
     EK_pub = alice.perform_x3dh_start(bob.IK.public_key(), bob.SPK.public_key())
 
@@ -199,12 +227,3 @@ if __name__ == "__main__":
     print(ciphertext)
     plaintext = DECRYPT(alice.SK, ciphertext, b'')
     print(plaintext)
-    '''
-    alice = User()
-    bob = User()
-
-    alice.dh(bob.DHs[1])
-    bob.dh(alice.DHs[1])
-    
-    print(alice.SK, bob.SK)
-    '''
