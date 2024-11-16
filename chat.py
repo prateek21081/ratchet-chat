d4b0c8746338c4d2fcc44bdb46f0146bae3dce53
Prateek Kumar <prateek21081@iiitd.ac.in>
2024-11-16 19:10:46 +0530

    improve chat interface

--- chat.py
+++ chat.py
@@ -14,10 +14,14 @@ parser.add_argument('--host-port', type=int, default=8000)
 parser.add_argument('--peer-port', type=int, default=8000)
 args = parser.parse_args()
 
+user = User()
+
 host = SimpleXMLRPCServer((args.host_ip, args.host_port), allow_none=True, use_builtin_types=True, logRequests=False)
 host_thread = Thread(target=host.serve_forever)
 host_thread.start()
-user = User()
+
+peer = ServerProxy(f"http://{args.peer_ip}:{args.peer_port}", allow_none=True, use_builtin_types=True)
+peer_connected = False
 
 @host.register_function()
 def get_dh_pub() -> bytes:
@@ -26,54 +30,59 @@ def get_dh_pub() -> bytes:
 @host.register_function()
 def init_ratchet():
     user.ratchet_init()
+    global peer_connected
+    peer_connected = True
 
 @host.register_function()
 def send_message(header: dict, ciphertext: bytes):
     _header = HEADER((user.DHs[0], BYTES_TO_KEY(header['dh'])), header['pn'], header['n'])
     plaintext = user.ratchet_decrypt(_header, ciphertext, b'')
-    print(f"(peer) {plaintext}")
+    print("\rpeer: " + plaintext.decode())
+    print("\rself: ", end='')
 
 @host.register_function()
 def send_msg(plaintext: bytes):
-    print(f"(peer) {plaintext}")
+    print("\rpeer: " + plaintext.decode())
+    print("\rself: ", end='')
 
 @host.register_function()
 def ping():
     return True
 
-peer = ServerProxy(f"http://{args.peer_ip}:{args.peer_port}", allow_none=True, use_builtin_types=True)
+def connect_to_peer():
+    DHr = BYTES_TO_KEY(peer.get_dh_pub())
+    if not isinstance(DHr, dh.DHPublicKey):
+        raise TypeError() 
+    global user, peer_connected
+    user = User(DHr)
+    user.ratchet_init()
+    peer.init_ratchet()
+    peer_connected = True
 
-print("Waiting for peer to be available...")
+class ChatShell(cmd.Cmd):
+    prompt = 'self: '
+
+    def default(self, line: str) -> None:
+        if not peer_connected: connect_to_peer()
+        header, ciphertext = user.ratchet_encrypt(bytes(line, encoding='utf-8'), b'')
+        peer.send_message(header.state(), ciphertext)
+
+    def do_plainmsg(self, arg):
+        peer.send_msg(bytes(arg, encoding='utf-8'))
+
+    def do_ping(self, arg):
+        print(peer.ping())
+
+    def do__exit(self):
+        exit()
+
+print("Waiting for peer to be available...", end='')
 while True:
     try:
         peer.ping()
         break
     except:
         pass
-
-class ChatShell(cmd.Cmd):
-    prompt = '(chat) '
-
-    def do_ping(self, arg):
-        print(peer.ping())
-
-    def do_connect(self, arg):
-        DHr = BYTES_TO_KEY(peer.get_dh_pub())
-        if not isinstance(DHr, dh.DHPublicKey):
-            raise TypeError() 
-        global user
-        user = User(DHr)
-        user.ratchet_init()
-        peer.init_ratchet()
-
-    def do_msg(self, arg):
-        peer.send_msg(bytes(arg, encoding='utf-8'))
-
-    def do_message(self, arg):
-        header, ciphertext = user.ratchet_encrypt(bytes(arg, encoding='utf-8'), b'')
-        peer.send_message(header.state(), ciphertext)
-
-    def do_exit(self):
-        exit()
+print("Connected!")
 
 ChatShell().cmdloop()
